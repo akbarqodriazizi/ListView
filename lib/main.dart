8a79bf5232a0f27dc196928ed42557b8a5b17714
akbarqodriazizi <aziziquqiu@gmail.com>
2023-05-22 15:33:49 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+// M. Akbar Qodri Azizi
+// 1102200011
+// CE 03 01
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    final List<String> entries = <String>['A', 'B', 'C'];
+    final List<int> colorCodes = <int>[600, 500, 100];
+
+    return Scaffold(
+      appBar: AppBar(title: Text("Hello list")),
+      body: ListView.builder(
+        padding: EdgeInsets.all(10),
+        itemCount: entries.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            height: 50,
+            color: Colors.amber[colorCodes[index]],
+            child: Center(child: Text(entries[index])),
+          );
+        },
+      ),
+    );
+  }
+}
